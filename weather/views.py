61b39f2fb3f406c3f90237337b493199a037bb68
Alexey Korablin <Alexey_Korablin@epam.com>
2019-07-23 18:32:14 +0300

    02_WeatherApp Completed

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,9 @@
 import requests
 from django.shortcuts import render
 
+from .models import City
+from .forms import CityForm
+
 # Create your views here.
 
 
@@ -8,21 +11,32 @@ def index(request):
     appid = '25f3763955aaaf6059e58f006fb85083'  # этот ключ генерируется при регистрации на сервиче
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'Ryazan'
-    res = requests.get(url.format(city)).json()  # метод .json() представляет данные в виде словаря
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    print(res)
+    form = CityForm()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+    # print(cities)
 
-    print(city_info)
+    all_cities = []
+
+    for city in cities:
+        # print(city)
+        res = requests.get(url.format(city)).json()  # метод .json() представляет данные в виде словаря
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+
+    # print(all_cities)
 
     context = {
-        'info': city_info
+        'all_info': all_cities,
+        'form': form
     }
 
     return render(request, 'weather/index.html', context)    # возвращает результат вызова функции render, одним из
